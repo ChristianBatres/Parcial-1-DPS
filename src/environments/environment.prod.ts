cb48bd52370d8e446e17b265abf65bdbe717c4f0
batres <cbatresaleman97@gmail.com>
2020-10-04 18:05:23 -0600

    Version final parcial 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB1dkHkiAz5z9VytIEmusZUAkLYCswIp0g",
+  authDomain: "veterinaria-2985c.firebaseapp.com",
+  databaseURL: "https://veterinaria-2985c.firebaseio.com",
+  projectId: "veterinaria-2985c",
+  storageBucket: "veterinaria-2985c.appspot.com",
+  messagingSenderId: "897678001971",
+  appId: "1:897678001971:web:65b87604bbb40eecee2046",
+  measurementId: "G-N8ZSB4MXV8"}
 };
